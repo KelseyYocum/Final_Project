4a69d616970710ca5cdf4660a2f2efbd16a1c154
Kelsey <kelsey@yocum.com>
2013-12-10 12:02:26 -0800

    modifications

--- model.py
+++ model.py
@@ -3,7 +3,7 @@
 
 
 import config
-import bcrypt
+import py-bcrypt
 from datetime import datetime
 import string
 
@@ -43,13 +43,13 @@ class User(Base, UserMixin):
 
 
     def set_password(self, password):
-        self.salt = bcrypt.gensalt()
+        self.salt = py-bcrypt.gensalt()
         password = password.encode("utf-8")
-        self.password = bcrypt.hashpw(password, self.salt)
+        self.password = py-bcrypt.hashpw(password, self.salt)
 
     def authenticate(self, password):
         password = password.encode("utf-8")
-        return bcrypt.hashpw(password, self.salt.encode("utf-8")) == self.password
+        return py-bcrypt.hashpw(password, self.salt.encode("utf-8")) == self.password
 
     def add_friend(self, *friends):
         for f in friends:
